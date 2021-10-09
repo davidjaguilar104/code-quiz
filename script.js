37b22ab6a572184f9cb6cacf5b4549457234531b
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-10-09 15:42:46 -0500

    added functioning timer

--- script.js
+++ script.js
@@ -3,8 +3,20 @@
 
 // variable pointing to the timer
 var timerEl = document.getElementById("timer");
+function countdown() {
+    var timeLeft = 75;
+
+    var timeInterval = setInterval(function() {
+        if(timeLeft > 0) {
+            timerEl.textContent = 'Time: ' + timeLeft;
+            timeLeft--;
+        }
+    }, 1000);
+}
+
+
 // variable pointing to the start quiz button 
 var startButtonEl = document.getElementById("start-quiz");
 startButtonEl.addEventListener("click" , function() {
-    
+    countdown();
 });
