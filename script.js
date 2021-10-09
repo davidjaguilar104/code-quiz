af85b96637d7c4b8748183d1aacd87acbb9b36d6
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-10-09 15:23:12 -0500

    added id's to button and time/added click listener

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+// variable pointing to the timer
+var timerEl = document.getElementById("timer");
+// variable pointing to the start quiz button 
+var startButtonEl = document.getElementById("start-quiz");
+startButtonEl.addEventListener("click" , function() {
+    
+});
